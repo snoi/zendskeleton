94575dc15d8593b5ef0c41c817d59bc4aad07333
shira <inos.54@gmail.com>
2015-01-21 17:07:32 +0700

    menu album

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,8 +15,8 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-//                        'controller' => 'Application\Controller\Index',
-                        'controller' => 'Album/Controller/Album',
+                        'controller' => 'Application\Controller\Index',
+//                        'controller' => 'Album/Controller/Album',
                         'action'     => 'index',
                     ),
                 ),
